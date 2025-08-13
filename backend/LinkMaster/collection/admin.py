70ea0ac58f2b28ace1d136b5e6b12a0e15662d27
harshitkandpal <harshitheroh5@gmail.com>
2025-08-13 11:42:30 +0530

    refactor: Customized the admin panel of collection

--- backend/LinkMaster/collection/admin.py
+++ backend/LinkMaster/collection/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from .models import Link, Collection
 
 # Register your models here.
+# lets modify the admin panel
 
-admin.site.register(Link)
-admin.site.register(Collection)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'url', 'description']
+    search_fields = ['title', 'url']
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description']
+    search_fields = ['name']
+
+admin.site.register(Link, LinkAdmin)
+admin.site.register(Collection, CollectionAdmin)
 
